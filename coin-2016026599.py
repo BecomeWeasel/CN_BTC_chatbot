98b7387eff75336978978176183adbae04887963
king10tech <hsw0194@gmail.com>
2017-10-11 14:34:38 +0900

    chatbot_btc implement

--- coin-2016026599.py
+++ coin-2016026599.py
@@ -14,13 +14,17 @@ from pprint import pprint
 
 locale.setlocale(locale.LC_ALL, '')
 
+
 # fill up your code!
 def GetBTC():
-    Time=datetime.datetime.now()
+    Time = datetime.datetime.now()
     currentTime = Time.strftime('%Y-%m-%d %H:%M')
     print(currentTime)
-    GetBitfinex()
-    GetBithumb()
+    USD=GetBitfinex()
+    KRW=GetBithumb()
+
+    bot.sendMessage(455389867,"{0}\nBTC KRW is ₩ {1} from Bithumb\nBTC USD is $ {2} from Bitfinex".format(currentTime,KRW,USD))
+
 
 
 def GetBithumb():
@@ -28,17 +32,19 @@ def GetBithumb():
     R_bithumb = requests.post(bithumbUrl)
     BTC_bithumb = json.loads(
         R_bithumb.text)  # R_bithumb.text ===str , so using json.loads, convert R_bithumb.text into dict format and assign to BTC_bitnumb(===dict)
-    print("BTC KRW is ₩ %.3d from Bithumb" % float(BTC_bithumb["data"]["average_price"]))
+    return BTC_bithumb["data"]["average_price"]
+
 
 def GetBitfinex():
     bitfinexUrl = "https://min-api.cryptocompare.com/data/price"
     bitfinexParams = {'fsym': 'BTC', 'tsyms': 'USD', 'e': 'Bitfinex'}
     R_bitfinex = requests.post(bitfinexUrl, params=bitfinexParams)
     BTC_bitfinex = json.loads(R_bitfinex.text)
-    print("BTC USD is $ %.3d from Bitfinex" % float(BTC_bitfinex["USD"]))
+    return BTC_bitfinex["USD"]
 
 
 if __name__ == "__main__":
+    bot = telepot.Bot('458359558:AAE3CSoLc67Y8-R7IT7deuuZB8qnL5NBct4')
     scheduler = BlockingScheduler()
     scheduler.add_job(GetBTC, 'interval', minutes=1)
     scheduler.start()
