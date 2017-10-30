2233226ce8b8a4ff0a23466d99f5c84beb550122
king10tech <hsw0194@gmail.com>
2017-10-31 01:17:48 +0900

    csv implementation

--- coin-2016026599.py
+++ coin-2016026599.py
@@ -19,16 +19,47 @@ def GetBTC():
     currentTime = Time.strftime('%Y-%m-%d %H:%M')
     print(currentTime)
     USD = GetBitfinex()
-    KRW = GetBithumb()
+    KRW = float(GetBithumb())
+    KRW_MOVE = 0
+    USD_MOVE = 0
+    prevValues = []
+    currentKRWValues = []
+    currentUSDValues = []
+    isInitial = True
+    READ = open('BTC.csv', 'r+')
+    reader = csv.reader(READ, delimiter=',')
+    for row in reader:
+        if (row != None):
+            prevValues.append(row)
+            isInitial = False
+        else:
+            break
+    READ.close()
+
+    currentKRWValues.append(str(KRW))
+    currentUSDValues.append(str(USD))
+
+    if isInitial is False:
+        KRW_MOVE = float(currentKRWValues[0]) - float(prevValues[0][0])
+        USD_MOVE = float(currentUSDValues[0]) - float(prevValues[1][0])
+
+    WRITE = open('BTC.csv', 'w')
+    writer = csv.writer(WRITE, quotechar=',', delimiter=',')
+    writer.writerow(currentKRWValues)
+    writer.writerow(currentUSDValues)
+
+    WRITE.close()
+
     bot.sendMessage(455389867,
-                    "{0}\nBTC KRW is ₩ {1} from Bithumb\nBTC USD is $ {2} from Bitfinex".format(currentTime, KRW, USD))
+                    "{0}\nBTC KRW is ₩ {1}\nChange : {2} from Bithumb\nBTC USD is $ {3}\nChange : {4} from Bitfinex".format(
+                        currentTime, KRW, KRW_MOVE, USD, USD_MOVE))
 
 
 def GetBithumb():
     bithumbUrl = "https://api.bithumb.com/public/ticker/BTC"
     R_bithumb = requests.post(bithumbUrl)
     BTC_bithumb = json.loads(
-        R_bithumb.text)  # R_bithumb.text ===str , so using json.loads, convert R_bithumb.text into dict format and assign to BTC_bitnumb(===dict)
+        R_bithumb.text)
     return BTC_bithumb["data"]["average_price"]
 
 
@@ -42,6 +73,11 @@ def GetBitfinex():
 
 if __name__ == "__main__":
     bot = telepot.Bot('458359558:AAE3CSoLc67Y8-R7IT7deuuZB8qnL5NBct4')
+    directory = os.getcwd()
+    directory += "/BTC.csv"
+    if not os.path.exists(directory):
+        csvfile = open("BTC.csv", 'w+')
+        csvfile.close()
     scheduler = BlockingScheduler()
     scheduler.add_job(GetBTC, 'interval', minutes=1)
     scheduler.start()
